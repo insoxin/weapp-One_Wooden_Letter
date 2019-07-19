294b08f3507898a396d22f0300a01727bb2e97d7
Redy <192666378@qq.com>
2019-07-19 08:58:41 +0800

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-App({
-    onLaunch: function() {
-        var n = wx.getStorageSync("logs") || [];
-        n.unshift(Date.now()), wx.setStorageSync("logs", n);
-    },
-    globalData: {
-        userInfo: null
-    }
-});
